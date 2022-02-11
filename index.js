fa5b670a7461506c04d7a7e32215c2a3e5469f7b
jgabriel <jgabrielcastillo@gmail.com>
2022-02-10 22:49:39 -0500

    creacion de rutas

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('my app');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send ('Hola mi server en express')
+})
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send ('Hola soy una nueva ruta')
+})
+
+app.get('/productos', (req, res) => {
+  res.json ({
+    name: 'producto1',
+    price: 1000
+  })
+})
+
+app.listen(port, () => {
+  console.log('Mi port' + port);
+})
